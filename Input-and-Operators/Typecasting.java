49ef17a83433121cc7c24b4d7335e1d067a64663
khushboodixit03 <165786244+khushboodixit03@users.noreply.github.com>
2025-12-16 11:40:58 +0530

    Solved Typecasting

---
+++ Input-and-Operators/Typecasting.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class typecaste{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        double y = (double)x;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
