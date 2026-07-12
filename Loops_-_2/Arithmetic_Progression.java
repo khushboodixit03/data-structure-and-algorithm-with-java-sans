735c8ad2dfa43ebe69ac1d1e22ab307c4f9074e0
khushboodixit03 <165786244+khushboodixit03@users.noreply.github.com>
2026-07-12 17:10:18 +0530

    Solved Arithmetic Progression

---
+++ Loops_-_2/Arithmetic_Progression.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        int i = 1;
+        while(i <= count){
+            System.out.print(i + " ");
+            i = i+2;
+        }
+    }
+}
